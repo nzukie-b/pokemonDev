34bab8373b6f0ecdbb94b58a45bac134b5d9e95c
nzukie <nzukie.b@husky.neu.edu>
2019-06-08 15:58:30 -0400

    Render pokemon

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import service from '../services/PokeAPIService'
 
-const PokeAPIServie = service.getInstance()
+const PokeAPIServie = service.getInstance();
+
+renderPokemon = (text) => {
+    PokeAPIServie.findPokemon(text)
+    .then(pokeList => pokeList.map(pokemon => {
+        <h1>{pokemon.name}</h1>
+    }))
+}
+
+
+
 
 const SearchPage = () => {
     return (
         <div className="container">
             <h1>Search Page</h1>
-            <input className="form-control" placeholder="Enter Pokemon Name"></input>
-            
+            <input className="form-control" placeholder="Enter Pokemon Name" 
+            onChange={(e) => {
+                renderPokemon(e.currentTarget.value);
+            }}></input>
+            {/* <button onClick={(e) => {
+                PokeAPIServie
+            }} */}
+
+
         </div>
         )
 }

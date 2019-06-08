bfd83abf31a6216fcffa913b6855cb90aec14d62
Benjamin Herzberg <herzberg.b@husky.neu.edu>
2019-06-08 15:49:13 -0400

    added servie to serch page

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import service from '../services/PokeAPIService'
+
+const PokeAPIServie = service.getInstance()
 
 const SearchPage = () => {
     return (<h1>Search Page</h1>)

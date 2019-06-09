9ab0b2a2e50c470b8733c6742ce5a1758e6dfa1a
nzukie <nzukie.b@husky.neu.edu>
2019-06-09 12:22:46 -0400

    Info page back button and general styling

--- src/components/Serch.js
+++ src/components/Serch.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import service from "../services/PokeAPIService"
+const pokeService = service.getInstance();
 
 const Search = ({ findPokemon, filterAllByPokemon, filteredPokemon }) => {
     return (
         <div>
-            <h1>Search Page</h1>
+            <h2>Search Page</h2>
             <input className="form-control"
                 placeholder="Pikachu"
                 onChange={(e) => {
@@ -12,19 +14,23 @@ const Search = ({ findPokemon, filterAllByPokemon, filteredPokemon }) => {
                         filterAllByPokemon(e.target.value.toLowerCase())
                     }
                 }} />
-            {filteredPokemon.map((pok) => {
-                const linkVar = "/pokemon/" + pok.name
-                return (<div>
-                    <Link
-                        to={linkVar}
-                        onClick={(e) => {
-                            findPokemon(pok.name)
-                            
-                            }}>
-                        {pok.name}
-                    </Link>
-                </div>)
-            })}
+            <div className="row mt-3">
+                {filteredPokemon.map((pok) => {
+                    const linkVar = "/pokemon/" + pok.name
+                    return (
+                        <div className="col-2 mb-1 px-1">
+                            <Link className="btn btn-outline-info btn-block"
+                                to={linkVar}
+                                onClick={() => {
+                                    findPokemon(pok.name)
+                                }}>
+                                {pok.name}
+                            </Link>
+                        </div>
+                    )
+                }
+                )}
+            </div>
         </div>
     )
 }

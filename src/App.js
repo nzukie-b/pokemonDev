4e3d6f758229ebca5add78ca5c9ae7a377a8b8d1
Benjamin Herzberg <herzberg.b@husky.neu.edu>
2019-06-17 15:49:01 -0400

    basic user profile page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 import SearchPage from "./components/SearchPage";
 import 'bootstrap/dist/css/bootstrap.css'
+import UserProfilePage from "./components/User/UserProfilePage";
 
 function App() {
   return (
-    <SearchPage/>
+    <UserProfilePage/>
   );
 }
 
--- src/components/User/UserProfilePage.js
+++ src/components/User/UserProfilePage.js
@@ -1,5 +1,38 @@
+import React from "react"
 
 const UserProfilePage = () => {
+    return (<form className="form-group">
+        <h1>Profile</h1>
+        <div>
+            <label for="userName">Username</label>
+            <input type="text"
+                   placeholder="Username"
+                   className="form-control"
+                   id="userName"/>
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password"
+                   placeholder="pass"
+                   className="form-control"
+                   id="password"/>
+        </div>
+        <div>
+            <label for="first">First Name</label>
+            <input type="text"
+                   placeholder="First Name"
+                   className="form-control"
+                   id="first"/>
+        </div>
+        <div>
+            <label for="last">Last Name</label>
+            <input type="text"
+                   placeholder="Last Name"
+                   className="form-control"
+                   id="last"/>
+        </div>
+
+    </form>)
 
 }
 

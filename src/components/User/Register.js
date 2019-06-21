245860ff59723ab843c816d2475bab79f42d9a63
Benjamin Herzberg <herzberg.b@husky.neu.edu>
2019-06-21 00:07:51 -0400

    heroku url

--- src/components/User/Register.js
+++ src/components/User/Register.js
@@ -77,7 +77,7 @@ export default class Register extends React.Component {
                                     newUser: {}
                                 })
                             )
-                            
+
                         }}>Create User
                     </div>
                 </form>

b112e0a1afed93b69088dcbda2d316d57cc70db2
nzukie <nzukie.b@husky.neu.edu>
2019-06-22 12:35:09 -0400

    pokeBall icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,30 @@
 import React from 'react'
-import {Link} from "react-router-dom";
-import {LoggedInUserContainer} from "../containers/UserContainer";
+import { Link } from "react-router-dom";
+import { LoggedInUserContainer } from "../containers/UserContainer";
 
 const NavBar = () => {
-    return (<nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+    var imageStyle = {
+        width: '30px',
+        height: '30px'
+    }
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            {/* <a className="navbar-brand" href="#">Navbar</a> */}
+            <img src="https://ui-ex.com/images/pokeball-transparent-8-bit.png" alt="Pokémon" style={imageStyle}></img>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-                <Link className="nav-item nav-link" to="/home">Home <span className="sr-only">(current)</span></Link>
-                <Link className="nav-item nav-link" to="/search">Search</Link>
-                <Link className="nav-item nav-link" to="/training">Training</Link>
-                <LoggedInUserContainer/>
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <Link className="nav-item nav-link" to="/home">Home <span className="sr-only">(current)</span></Link>
+                    <Link className="nav-item nav-link" to="/search">Search</Link>
+                    <Link className="nav-item nav-link" to="/training">Training</Link>
+                    <LoggedInUserContainer />
+                </div>
             </div>
-        </div>
-    </nav>)
+        </nav>
+    )
 }
 
 export default NavBar

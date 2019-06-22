0a83c4c06ab8c51a3368dfd4f9f26026cbf60386
Benjamin Herzberg <herzberg.b@husky.neu.edu>
2019-06-22 18:01:21 -0400

    PokeDev

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,8 +9,8 @@ const NavBar = () => {
     }
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            {/* <a className="navbar-brand" href="#">Navbar</a> */}
             <img src="https://ui-ex.com/images/pokeball-transparent-8-bit.png" alt="Pokémon" style={imageStyle}></img>
+            <a className="navbar-brand">PokeDev</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>

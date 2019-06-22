f71556fbe94e5d49a0961e4cf3fe7af972aba794
Benjamin Herzberg <herzberg.b@husky.neu.edu>
2019-06-22 15:19:25 -0400

    Fixed the 10 ten issue

--- src/components/User/UserList.js
+++ src/components/User/UserList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import CollectedPokemon from "../Pokemon/CollectedPokemon";
 
 export default class UserList extends React.Component {
@@ -17,7 +17,7 @@ export default class UserList extends React.Component {
             return (
                 <div>
                     <h4>Hi {this.props.user.firstName.charAt(0).toUpperCase() +
-                        this.props.user.firstName.slice(1)}!</h4>
+                    this.props.user.firstName.slice(1)}!</h4>
                     <h5>You've collected {collectionSize} out of 964 Pokemon</h5>
                 </div>
             )
@@ -46,14 +46,14 @@ export default class UserList extends React.Component {
             return (
                 <div className="my-3 border rounded p-2">
                     <Link className="btn btn-block btn-outline-primary"
-                        to={`/profile/${this.props.user.username}`}
-                        onClick={() => this.props.findUserProfile(this.props.user.id)}>
+                          to={`/profile/${this.props.user.username}`}
+                          onClick={() => this.props.findUserProfile(this.props.user.id)}>
                         {"User Profile:" + this.props.user.username}
                     </Link>
                     <CollectedPokemon loggedIn={true}
-                        user={this.props.user}
-                        collectedPokemon={this.props.user.collectedPokemon}
-                        training={false}
+                                      user={this.props.user}
+                                      collectedPokemon={this.props.user.collectedPokemon}
+                                      training={false}
                     />
                     <br></br>
                 </div>
@@ -62,28 +62,25 @@ export default class UserList extends React.Component {
     }
 
     showTopTenTeams = () => {
-        var allUsers = this.props.users.sort((user1, user2) =>
+        return this.props.users.sort((user1, user2) =>
             this.getTeamLevelTotal(user2) - this.getTeamLevelTotal(user1))
-        if (allUsers.length > 10) {
-            allUsers = allUsers.splice(10)
-        }
-        return allUsers.map(user => {
-            return (
-                <div className="my-3 border rounded p-2">
-                    <Link className="btn btn-block btn-outline-primary"
-                        to={`/profile/${user.username}`}
-                        onClick={() => this.props.findUserProfile(user.id)}>
-                         {"User Profile:" + user.username}
-                    </Link>
-                    <CollectedPokemon loggedIn={true}
-                        user={user}
-                        collectedPokemon={user.collectedPokemon}
-                        training={false}
-                    />
-                    <br></br>
-                </div>
-            )
-        })
+            .slice(0,10).map(user => {
+                return (
+                    <div className="my-3 border rounded p-2">
+                        <Link className="btn btn-block btn-outline-primary"
+                              to={`/profile/${user.username}`}
+                              onClick={() => this.props.findUserProfile(user.id)}>
+                            {"User Profile:" + user.username}
+                        </Link>
+                        <CollectedPokemon loggedIn={true}
+                                          user={user}
+                                          collectedPokemon={user.collectedPokemon}
+                                          training={false}
+                        />
+                        <br></br>
+                    </div>
+                )
+            })
     }
 
     render() {
